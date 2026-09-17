d159bc73c3865c894cac6cdb2f46ddd9e746c3a3
Zen <zenbsswong@gmail.com>
2026-09-17 20:56:22 +0800

    example

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ async def webhook(request: Request):
             elif "help" in user_message.lower():
                 reply_text = "Sure, what do you need help with?"
             elif "จอง Private class ka" in user_message:
-                reply_text = "ได้เลยค่า กรุณาแจ้งวันและเวลาที่สนใจไว้เลยนะคะ (พรุ่งนี้ 7 โมงเช้า)"
+                reply_text = "ได้เลยค่า กรุณาแจ้งวันและเวลาที่สนใจไว้เลยนะคะ (วัน/เดือน ชม:นาที เช่น 18/09 10:00)"
 
             # --- Reply to LINE ---
             requests.post(
